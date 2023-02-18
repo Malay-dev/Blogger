362d98407baef5cd37c388a46de1b4be74167053
Malay <malaykumar2003@gmail.com>
2023-02-18 23:41:23 +0530

    Feat: Mongoose setup

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,15 @@
 import express from "express";
 
-import { askQuestion } from "../controllers/question.js";
-import { getAllQuestions } from "../controllers/question.js";
-import { deleteQuestion } from "../controllers/question.js";
-import { voteQuestion } from "../controllers/question.js";
+import { postBlog } from "../controllers/post.js";
+import { getAllBlogs } from "../controllers/post.js";
+import { deleteBlog } from "../controllers/post.js";
+import { voteBlog } from "../controllers/post.js";
 
 import auth from "../middlewares/auth.js";
 
 const postRoutes = express.Router();
-postRoutes.post("/Ask", auth, askQuestion);
-postRoutes.get("/get", getAllQuestions);
-postRoutes.delete("/delete/:id", auth, deleteQuestion);
-postRoutes.patch("/vote/:id", auth, voteQuestion);
+postRoutes.post("/post", auth, postBlog);
+postRoutes.get("/get", getAllBlogs);
+postRoutes.delete("/delete/:id", auth, deleteBlog);
+postRoutes.patch("/vote/:id", auth, voteBlog);
 export default postRoutes;
