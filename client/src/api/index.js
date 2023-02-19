5a721bd4b1bc1c46eb78f2b82993a7efa38e12ff
Malay <malaykumar2003@gmail.com>
2023-02-19 19:05:33 +0530

    Fix: update server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://blogger-node-server.onrender.com", //http://localhost:5000
 });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
